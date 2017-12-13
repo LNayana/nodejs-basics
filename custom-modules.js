199d80229c55b5aced36e03dc1639beef0b7cada
LNayana <lokhandenayana22@gmail.com>
2017-12-13 17:53:51 +0530

    created and exported custom modules

---
+++ custom-modules.js
@@ -0,0 +1,6 @@
+var myCalculator = require('./libs/basic-maths');
+
+console.log("Addition is: ", myCalculator.addition(2, 2));
+console.log("Subtraction is: ", myCalculator.subtraction(2, 2));
+console.log("Multiplication is: ", myCalculator.multiplication(2, 2));
+console.log("Division is: ", myCalculator.division(2, 2));
---
+++ libs/basic-maths.js
@@ -0,0 +1,4 @@
+exports.addition = (no1, no2) => no1 + no2;
+exports.subtraction = (no1, no2) => no1 - no2;
+exports.multiplication = (no1, no2) => no1 * no2;
+exports.division = (no1, no2) => no1 / no2;
